c356b49453475191367f3bc89f9f748ad2c4b09b
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-11-24 19:26:35 -0300

    create form register categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/categories', (req, res) => {
-    res.send("Rota de categoria");
+router.get("/admin/categories/new", (req, res)=> {
+    res.render('admin/categories/new');
 });
-
-router.get('/categories/adm', (req, res) => {
-    res.send("categories administrador");
-})
 
 module.exports = router;
